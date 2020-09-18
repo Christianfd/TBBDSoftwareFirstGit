e74ca74cd659561f9e2bc1ba73f033f8262fe623
Christian Davis <skole.davis@live.dk>
2020-09-18 12:48:11 +0200

    Renamed Script

---
+++ hello.js
@@ -0,0 +1,15 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: procesc.stdin,
+    output: process.stdout
+});
+
+rl.question("Whait is your name?", function(name){
+    console.log(`Hello ${name}, welcome to my repository`);
+    rl.close();
+});
+
+rl.on("close",function(){
+    console.log("\nBYE BYE !!!");
+    process.exit(0);
+});
